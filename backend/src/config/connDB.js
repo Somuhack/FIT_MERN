c65f77142747255c2623cc3fb91b037b3bb08231
Somuhack <somnathbarik516@gmail.com>
2025-08-14 11:35:42 +0530

    main index.js change

--- backend/src/config/connDB.js
+++ backend/src/config/connDB.js
@@ -3,10 +3,9 @@ const URI="mongodb+srv://som:12345@cluster0.tzt1r.mongodb.net/FIT_MERN?retryWrit
 const connDB = async()=>{
     try {
         await mongoose.connect(URI);
-    console.log("DB connected successfull");
+        console.log("DB connected successfull");
     } catch (error) {
         console.log("DB connection failed", error);
-        
     }
 }
 module.exports = connDB
